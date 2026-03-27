0e91e4e6a67df9654468b3a52c29fb295a5d5347
Jan Remes <ciakval@gmail.com>
2026-03-27 15:41:52 -0700

    UI: update homepage

--- SkaRe/context_processors.py
+++ SkaRe/context_processors.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from .permissions import is_infodesk
 
 
 def version_info(request):
     return {
         'VERSION': settings.VERSION,
         'BUILD_ID': settings.BUILD_ID,
+        'user_is_infodesk': is_infodesk(request.user) if request.user.is_authenticated else False,
     }
